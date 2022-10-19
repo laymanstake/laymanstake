7072a0f71a99718a045d5d10851e3c0badaa98a8
laymanstake <nitish@nitishkumar.net>
2022-10-19 17:55:08 +0530

    First copy

---
+++ SSLCertMonitor.ps1
@@ -0,0 +1,55 @@
+# Author : Nitish Kumar
+# To Automate SSL certificate Monitoring
+# version 1.0 | 19/10/2022
+
+# Function to send email
+function EmailAlert () {
+	[CmdletBinding()]
+	param(
+		[parameter(mandatory=$true)]$RecipientAddressTo,		
+		[parameter(mandatory=$true)]$SenderAddress,
+		[parameter(mandatory=$true)]$SMTPServer,		
+		[parameter(mandatory=$true)]$Subject,
+		[parameter(mandatory=$true)]$Body,
+		[parameter(mandatory=$false)]$SMTPServerPort="25",		
+		[parameter(mandatory=$false)]$RecipientAddressCc,
+		[parameter(mandatory=$false)][switch]$SMTPServerSSL=$false
+	)
+
+	if ($RecipientAddressCc){
+		try{
+			$email = @{
+				From = $SenderAddress
+				To = $RecipientAddressTo
+				Cc = $RecipientAddressCc
+				Subject = $Subject
+				Body = $Body
+				SmtpServer = $SMTPServer
+				Port = $SMTPServerPort
+				UseSsl = $SMTPServerSSL
+			}		
+			Send-MailMessage @email 
+		}
+		Catch{
+			Throw $_.exception.message 
+		}
+	}
+	else {
+		try{
+			$email = @{
+				From = $SenderAddress
+				To = $RecipientAddressTo
+				Subject = $Subject
+				Body = $Body
+				SmtpServer = $SMTPServer			
+				Port = $SMTPServerPort
+				UseSsl = $SMTPServerSSL
+			}		
+			Send-MailMessage @email 
+		}
+		Catch{
+			Throw $_.exception.message 
+		}# Action when all if and elseif conditions are false #>
+	}
+
+}
