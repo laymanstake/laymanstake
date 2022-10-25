a589c37b7e65bbab9fe446e3bbdbc655598ef90a
laymanstake <nitish@nitishkumar.net>
2022-10-25 13:21:06 +0530

    Added remove service option and removed sudo from inside the script

--- PrometheusGrafanaSetup.sh
+++ PrometheusGrafanaSetup.sh
@@ -69,32 +69,32 @@ Install-Executables() {
         then
             # Make prometheus user
             echo "${green} Creating prometheus user ...${reset}"
-            sudo useradd -rs /bin/false -c "Prometheus User" prometheus
+            useradd -rs /bin/false -c "Prometheus User" prometheus
             tar xzf $HOME/executables/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz -C $HOME/executables/
 
             cd $HOME/executables/
 
             # Make directories and dummy files necessary for prometheus
-            sudo mkdir /etc/prometheus
-            sudo mkdir /var/lib/prometheus
-            sudo touch /etc/prometheus/prometheus.yml        
+            mkdir /etc/prometheus
+            mkdir /var/lib/prometheus
+            touch /etc/prometheus/prometheus.yml        
 
             # permissions setup
-            sudo chown -R prometheus:prometheus /etc/prometheus        
+            chown -R prometheus:prometheus /etc/prometheus        
             
-            sudo cp prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus /usr/local/bin/
-            sudo cp prometheus-${PROMETHEUS_VERSION}.linux-amd64/promtool /usr/local/bin/
-            sudo cp -r prometheus-${PROMETHEUS_VERSION}.linux-amd64/consoles /etc/prometheus
-            sudo cp -r prometheus-${PROMETHEUS_VERSION}.linux-amd64/console_libraries /etc/prometheus
-            sudo cp -r prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus.yml /etc/prometheus
+            cp prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus /usr/local/bin/
+            cp prometheus-${PROMETHEUS_VERSION}.linux-amd64/promtool /usr/local/bin/
+            cp -r prometheus-${PROMETHEUS_VERSION}.linux-amd64/consoles /etc/prometheus
+            cp -r prometheus-${PROMETHEUS_VERSION}.linux-amd64/console_libraries /etc/prometheus
+            cp -r prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus.yml /etc/prometheus
 
             # permissions setup
-            sudo chown prometheus:prometheus /var/lib/prometheus
-            sudo chown -R prometheus:prometheus /etc/prometheus/consoles
-            sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
-            sudo chown prometheus:prometheus /usr/local/bin/prometheus
-            sudo chown prometheus:prometheus /usr/local/bin/promtool
-            sudo chown prometheus:prometheus /etc/prometheus/prometheus.yml
+            chown prometheus:prometheus /var/lib/prometheus
+            chown -R prometheus:prometheus /etc/prometheus/consoles
+            chown -R prometheus:prometheus /etc/prometheus/console_libraries
+            chown prometheus:prometheus /usr/local/bin/prometheus
+            chown prometheus:prometheus /usr/local/bin/promtool
+            chown prometheus:prometheus /etc/prometheus/prometheus.yml
 
             # update service configuration
             cat >/etc/systemd/system/prometheus.service <<EOL
@@ -115,14 +115,14 @@ EOL
 
             # setup service
             echo "${green} Enabling and starting prometheus service ... ${reset}"
-            sudo systemctl daemon-reload
-            sudo systemctl enable prometheus
-            sudo systemctl start prometheus
+            systemctl daemon-reload
+            systemctl enable prometheus
+            systemctl start prometheus
 
             # create firewall rules
             echo "${green} Creating firewall rules for prometheus service ... ${reset}"
-            sudo firewall-cmd --zone=public --add-port=9090/tcp --permanent
-            sudo systemctl reload firewalld
+            firewall-cmd --zone=public --add-port=9090/tcp --permanent
+            systemctl reload firewalld
 
             echo "${green}If all went well, then you should be able to access Prometheus on http://localhost:9090 ...${reset}"
 
@@ -142,13 +142,13 @@ EOL
         then
             # Make node_exporter user
             echo "${green} Creating nodeusr user ...${reset}"
-            sudo useradd -rs /bin/false -c "Node Exporter User" nodeusr        
+            useradd -rs /bin/false -c "Node Exporter User" nodeusr        
             tar xzf $HOME/executables/node_exporter-${NODE_VERSION}.linux-amd64.tar.gz -C $HOME/executables/
 
             cd $HOME/executables/
 
-            sudo cp node_exporter-${NODE_VERSION}.linux-amd64/node_exporter /usr/local/bin/
-            sudo chown nodeusr:nodeusr /usr/local/bin/node_exporter
+            cp node_exporter-${NODE_VERSION}.linux-amd64/node_exporter /usr/local/bin/
+            chown nodeusr:nodeusr /usr/local/bin/node_exporter
 
             # update service configuration
             cat >/etc/systemd/system/node_exporter.service <<EOL
@@ -167,15 +167,15 @@ EOL
 
             # setup service
             echo "${green} Enabling and starting node_exporter service ... ${reset}"
-            sudo systemctl daemon-reload
-            sudo systemctl enable node_exporter
-            sudo systemctl start node_exporter
+            systemctl daemon-reload
+            systemctl enable node_exporter
+            systemctl start node_exporter
 
             # create firewall rules
             echo "${green} Creating firewall rules for node_exporter service (for windows one in addition) ... ${reset}"
-            sudo firewall-cmd --zone=public --add-port=9100/tcp --permanent 
-            sudo firewall-cmd --zone=public --add-port=9182/tcp --permanent # creating one of Windows Node in advance, you can comment the line if not needed
-            sudo systemctl reload firewalld
+            firewall-cmd --zone=public --add-port=9100/tcp --permanent 
+            firewall-cmd --zone=public --add-port=9182/tcp --permanent # creating one of Windows Node in advance, you can comment the line if not needed
+            systemctl reload firewalld
 
             echo "${green}If all went well, then you should be able to access Node_exporter on http://localhost:9100 ...${reset}"
 
@@ -190,23 +190,95 @@ EOL
 
     if [[ "$*" == *"grafana"* ]]
     then
-        sudo yum install grafana -y -q
+        yum install grafana -y -q
 
         # setup service
         echo "${green} Enabling and starting Grafana service ... ${reset}"
-        sudo systemctl daemon-reload
-        sudo systemctl enable grafana-server
-        sudo systemctl start grafana-server
+        systemctl daemon-reload
+        systemctl enable grafana-server
+        systemctl start grafana-server
 
         # create firewall rules
         echo "${green} Creating firewall rules for Grafana service ... ${reset}"
-        sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
-        sudo systemctl reload firewalld
+        firewall-cmd --zone=public --add-port=3000/tcp --permanent
+        systemctl reload firewalld
 
         echo "${green}If all went well, then you should be able to access Grafana on http://localhost:3000 ...${reset}"
     fi
 }
 
+Remove-Services() {
+    # Remove Node Exporter
+    if [[ "$*" == *"node_exporter"* ]]
+    then
+        # Stop and remove Node Exporter Service
+        echo "${green} Stoping and removing Node Exporter Service and related files ... ${reset}"
+        systemctl stop node_exporter
+        systemctl disable node_exporter
+        rm -f /usr/local/bin/node_exporter
+        rm -f /etc/systemd/system/node_exporter.service
+        systemctl daemon-reload
+        systemctl reset-failed
+
+        # Remove firewall rule created
+        echo "${green} Removing Node Exporter related firewall rules ... ${reset}"
+        firewall-cmd --zone=public --remove-port=9100/tcp --permanent 
+        systemctl reload firewalld
+
+        # Remove created user for the service
+        echo "${green} Removing Node Exporter related user account ... ${reset}"
+        userdel -f nodeusr
+    fi
+
+    if [[ "$*" == *"prometheus"* ]]
+    then
+        # Stop and remove Prometheus Service
+        echo "${green} Stoping and removing Prometheus Service and related files ... ${reset}"
+        systemctl stop prometheus
+        systemctl disable prometheus
+        rm -f /usr/local/bin/prometheus
+        rm -f /usr/local/bin/promtool
+        rm -rf /var/lib/prometheus
+        rm -rf /etc/prometheus
+        rm -f /etc/systemd/system/prometheus.service
+        systemctl daemon-reload
+        systemctl reset-failed
+
+        # Remove firewall rule created
+        echo "${green} Removing Prometheus related firewall rules ... ${reset}"
+        firewall-cmd --zone=public --remove-port=9090/tcp --permanent 
+        systemctl reload firewalld
+
+        # Remove created user for the service
+        echo "${green} Removing Prometheus related user account ... ${reset}"
+        userdel -f prometheus
+    fi
+
+    if [[ "$*" == *"grafana"* ]]
+    then
+        # Stop and remove Prometheus Service
+        echo "${green} Stoping and removing Grafana Service and related files ... ${reset}"
+        systemctl stop grafana-server
+        systemctl disable grafana-server        
+        systemctl daemon-reload
+        systemctl reset-failed
+        yum remove grafana -y -q
+        
+        # Removing Grafana Dashboards and DB
+        echo "${red} Removing Grafana related firewall rules ... ${reset}"
+        rm -rf /var/lib/grafana
+
+        # Remove firewall rule created
+        echo "${green} Removing Grafana related firewall rules ... ${reset}"
+        firewall-cmd --zone=public --remove-port=3000/tcp --permanent 
+        systemctl reload firewalld
+    fi
+ 
+}
+
 # Ready up executables
-Get-Executables prometheus grafana node_exporter
-Install-Executables prometheus grafana node_exporter
+#Get-Executables prometheus grafana node_exporter
+#Install-Executables prometheus grafana node_exporter
+#Remove-Services node_exporter grafana
+
+# You would still need to add config in /etc/prometheus/prometheus.yml before you can see data from the node
