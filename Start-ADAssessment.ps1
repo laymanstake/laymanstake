b1e9700c831c91d0bacb85c01442f29da0ecc3d7
laymanstake <nitish@nitishkumar.net>
2023-06-17 18:45:03 +0530

    ADFS / AD Sync count added in message

--- Start-ADAssessment.ps1
+++ Start-ADAssessment.ps1
@@ -2030,7 +2030,7 @@ Function Get-ADForestDetails {
         $ADSyncDetail = $ADSyncDetail | Sort-Object * -Unique
         $ADSyncDetails += $ADSyncDetail
         
-        $message = "Lookup for ADFS ($($ADFSDetail.count)) / ADSync ($($ADSyncDetail.count)) server in domain: $Domain done."
+        $message = "Lookup for ADFS ($($ADFSDetail.SERVERNAME.count)) / ADSync ($($ADSyncDetail.SERVERNAME.count)) server in domain: $Domain done."
         New-BaloonNotification -title "Information" -message $message
         Write-Log -logtext $message -logpath $logpath        
         
