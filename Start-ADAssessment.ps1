964991c1086d7bfead069374849851762f0d6c06
laymanstake <nitish@nitishkumar.net>
2023-06-20 10:18:52 +0530

    formatting

--- Start-ADAssessment.ps1
+++ Start-ADAssessment.ps1
@@ -2400,7 +2400,7 @@ Write-Output "Menu:"
 "Option 3: Press any other key to Quit`n"
 
 $choice = Read-Host "Enter your choice: "
-Write-Output "`nDO NOT OPEN LOG FILE DURING RUN otherwise it would not record"
+Write-Output "`nDO NOT OPEN LOG FILE DURING RUN otherwise it would not record`n`n`n"
 
 switch ($choice) {
     '1' {
