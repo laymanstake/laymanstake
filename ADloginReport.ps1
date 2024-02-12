af9a923f0aba5db5288e49585a5ffca4abdc7aec
Nitish Kumar <93420705+laymanstake@users.noreply.github.com>
2024-02-12 14:06:43 +0530

    Update ADloginReport.ps1

--- ADloginReport.ps1
+++ ADloginReport.ps1
@@ -1,23 +1,24 @@
 $DCs =  (Get-ADDomainController -filter *).Name #("XXX","ABC")
-
 $Userlogon = @()
+$users = get-content c:\temp\users1.txt
 
-$users = get-content c:\temp\users01.txt
-
-foreach ($DC in $DCs){    
+foreach ($DC in $DCs){
+        Write-Host "Getting Report from $DC"
         ForEach($user in $users){
-            $userlogontemp = Get-ADUser $user -Properties lastlogon, lastlogondate -Server $DC | Sort-Object -Property SamAccountName
+            $userlogontemp = Get-ADUser -filter {mail -eq $user} -Properties EmployeeID, mail, lastlogon, lastlogontimestamp -Server $DC #| Sort-Object -Property SamAccountName
 
             $Info = [PSCustomObject]@{
                     SamAccountName = $userlogontemp.SamAccountName
-                    LastlogonDate = $userlogontemp.LastlogonDate
+                    mail = $userlogontemp.mail
+                    EmployeeID = $userlogontemp.EmployeeID
+                    lastlogontimestamp = $userlogontemp.lastlogontimestamp
                     Lastlogon = $userlogontemp.Lastlogon
+                    FinalLogon = ($userlogontemp.lastlogontimestamp, $userlogontemp.Lastlogon)| Measure-Object -Maximum | Select-Object -ExpandProperty Maximum
                     DCName = $DC
                 }
             $Userlogon += $Info
         }
-    
 }
 
-$LatestList = $Userlogon | Group-Object -Property SamAccountName | % {$_.group | Sort-Object -Property lastlogon | select -Last 1} | Select-Object SamAccountName, @{l = "Lastlogon"; e = { [DateTime]::FromFileTime($_.LastLogon) } }, lastlogonDate, DCName
-$LatestList | export-csv -nti c:\temp\userloginreport01.csv
+$LatestList = $Userlogon | Group-Object -Property SamAccountName | % {$_.group | Sort-Object -Property FinalLogon | select -Last 1} | Select-Object SamAccountName, mail, EmployeeID, @{l = "Lastlogon"; e = { [DateTime]::FromFileTime($_.LastLogon).tostring('dd/MM/yyyy_hh:mm:ss tt') } }, @{l = "LastlogonDate"; e = { [DateTime]::FromFileTime($_.LastLogontimestamp).tostring('dd/MM/yyyy_hh:mm:ss tt') } },@{l = "FinalLogon"; e = { [DateTime]::FromFileTime($_.FinalLogon).tostring('dd/MM/yyyy_hh:mm:ss tt') } }, DCName
+$LatestList | export-csv -nti c:\temp\userloginreport02.csv
